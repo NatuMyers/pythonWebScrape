1ac6377a78b679dcc479c68f6209354ec22f99d5
Natu Myers <me@natumyers.com>
2016-07-07 11:44:05 -0400

    first commit

---
+++ scrapper.py
@@ -0,0 +1,12 @@
+
+
+
+import urllib2
+from import html2text #formats HTML to markdown
+
+# read each line of the md
+for line in html2text(urllib2.urlopen("http://www.moviebodycounts.com/Braveheart.htm").read()).split("\n")
+    if "IMDb" in line:
+        print line.split("[IMDb]")
+    if "Film:" in line:
+        print line.split("[IMDb]")
